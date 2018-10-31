0a4e79876fc29ef595d549b8df6f684e6df32b46
Keyur <keyur@moesif.com>
2018-10-31 16:34:07 -0700

    Add: Update user and batch users functions
    Add: Update user and batch users functions
    Add: Update user / batch users test
    Update: REAMDE.md
    Bump version to 1.1.1

--- Moesif.Api/Controllers/ApiController.cs
+++ Moesif.Api/Controllers/ApiController.cs
@@ -99,6 +99,104 @@ namespace Moesif.Api.Controllers
         }
 
         /// <summary>
+        /// Update Single User API Call
+        /// </summary>
+        /// <param name="body">Required parameter: Example: </param>
+        /// <return>Returns the void response from the API call</return>
+        public void UpdateUser(UserModel body)
+        {
+            Task t = UpdateUserAsync(body);
+            Task.WaitAll(t);
+        }
+
+        /// <summary>
+        /// Update Single User API Call
+        /// </summary>
+        /// <param name="body">Required parameter: Example: </param>
+        /// <return>Returns the void response from the API call</return>
+        public async Task UpdateUserAsync(UserModel body)
+        {
+            //the base uri for api requestss
+            string _baseUri = Configuration.BaseUri;
+
+            //prepare query string for API call
+            StringBuilder _queryBuilder = new StringBuilder(_baseUri);
+            _queryBuilder.Append("/v1/users");
+
+            //validate and preprocess url
+            string _queryUrl = ApiHelper.CleanUrl(_queryBuilder);
+
+            //append request with appropriate headers and parameters
+            var _headers = new Dictionary<string, string>()
+            {
+                { "content-type", "application/json; charset=utf-8" }
+            };
+            _headers.Add("X-Moesif-Application-Id", Configuration.ApplicationId);
+
+            //append body params
+            var _body = ApiHelper.JsonSerialize(body);
+
+            //prepare the API call request to fetch the response
+            HttpRequest _request = ClientInstance.PostBody(_queryUrl, _headers, _body);
+
+            //invoke request and get response
+            HttpStringResponse _response = (HttpStringResponse) await ClientInstance.ExecuteAsStringAsync(_request);
+            HttpContext _context = new HttpContext(_request, _response);
+            //handle errors defined at the API level
+            base.ValidateResponse(_response, _context);
+
+        }
+
+        /// <summary>
+        /// Update multiple Users in a single batch (batch size must be less than 250kb)
+        /// </summary>
+        /// <param name="body">Required parameter: Example: </param>
+        /// <return>Returns the void response from the API call</return>
+        public void UpdateUsersBatch(List<UserModel> body)
+        {
+            Task t = UpdateUsersBatchAsync(body);
+            Task.WaitAll(t);
+        }
+
+        /// <summary>
+        /// Update multiple Users in a single batch (batch size must be less than 250kb)
+        /// </summary>
+        /// <param name="body">Required parameter: Example: </param>
+        /// <return>Returns the void response from the API call</return>
+        public async Task UpdateUsersBatchAsync(List<UserModel> body)
+        {
+            //the base uri for api requestss
+            string _baseUri = Configuration.BaseUri;
+
+            //prepare query string for API call
+            StringBuilder _queryBuilder = new StringBuilder(_baseUri);
+            _queryBuilder.Append("/v1/users/batch");
+
+            //validate and preprocess url
+            string _queryUrl = ApiHelper.CleanUrl(_queryBuilder);
+
+            //append request with appropriate headers and parameters
+            var _headers = new Dictionary<string, string>()
+            {
+                { "content-type", "application/json; charset=utf-8" }
+            };
+            _headers.Add("X-Moesif-Application-Id", Configuration.ApplicationId);
+
+            //append body params
+            var _body = ApiHelper.JsonSerialize(body);
+
+            //prepare the API call request to fetch the response
+            HttpRequest _request = ClientInstance.PostBody(_queryUrl, _headers, _body);
+
+            //invoke request and get response
+            HttpStringResponse _response = (HttpStringResponse)await ClientInstance.ExecuteAsStringAsync(_request);
+            HttpContext _context = new HttpContext(_request, _response);
+            //handle errors defined at the API level
+            base.ValidateResponse(_response, _context);
+
+        }
+
+        /// <summary>
         /// Add multiple API Events in a single batch (batch size must be less than 250kb)
         /// </summary>
         /// <param name="body">Required parameter: Example: </param>
---
+++ Moesif.Api/Models/UserModel.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * MoesifAPI.PCL
+ *
+
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Moesif.Api;
+
+namespace Moesif.Api.Models
+{
+    public class UserModel: INotifyPropertyChanged
+    {
+        // These fields hold the values for the public properties.
+        private DateTime? modifiedTime;
+        private string sessionToken;
+        private string ipAddress;
+        private string userId;
+        private string userAgentString;
+        private object metadata;
+
+        /// <summary>
+        /// Time when request was made
+        /// </summary>
+        [JsonProperty("modified_time")]
+        public DateTime ModifiedTime
+        {
+            get
+            {
+                return this.modifiedTime??DateTime.UtcNow;
+            }
+            set
+            {
+                this.modifiedTime = value;
+                onPropertyChanged("ModifiedTime");
+            }
+        }
+
+        /// <summary>
+        /// End user's auth/session token
+        /// </summary>
+        [JsonProperty("session_token")]
+        public string SessionToken
+        {
+            get
+            {
+                return this.sessionToken;
+            }
+            set
+            {
+                this.sessionToken = value;
+                onPropertyChanged("SessionToken");
+            }
+        }
+
+        /// <summary>
+        /// End user's ip address
+        /// </summary>
+        [JsonProperty("ip_address")]
+        public string IpAddress
+        {
+            get
+            {
+                return this.ipAddress;
+            }
+            set
+            {
+                this.ipAddress = value;
+                onPropertyChanged("IpAddress");
+            }
+        }
+
+        /// <summary>
+        /// End user's user_id string from your app
+        /// </summary>
+        [JsonProperty("user_id")]
+        public string UserId
+        {
+            get
+            {
+                return this.userId;
+            }
+            set
+            {
+                this.userId = value;
+                onPropertyChanged("UserId");
+            }
+        }
+
+        /// <summary>
+        /// End user's user_agent_string string from your app
+        /// </summary>
+        [JsonProperty("user_agent_string")]
+        public string UserAgentString
+        {
+            get
+            {
+                return this.userAgentString;
+            }
+            set
+            {
+                this.userAgentString = value;
+                onPropertyChanged("UserAgentString");
+            }
+        }
+
+        /// <summary>
+        /// Metadata from your app, see documentation
+        /// </summary>
+        [JsonProperty("metadata")]
+        public object Metadata
+        {
+            get
+            {
+                return this.metadata;
+            }
+            set
+            {
+                this.metadata = value;
+                onPropertyChanged("Metadata");
+            }
+        }
+
+        /// <summary>
+        /// Property changed event for observer pattern
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises event when a property is changed
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property</param>
+        protected void onPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
