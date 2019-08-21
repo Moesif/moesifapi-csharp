91b63f7c93eedb0195795bbce1a546f02744b438
Keyur <keyur@moesif.com>
2019-08-21 13:49:45 -0700

    Add: CompanyId to UserModel
    Add: CompanyId to UserModel
    Refactor: Update User tests
    Refactor: Update README.md
    Bump version to 1.1.5

--- Moesif.Api.Test/ApiControllerTest.cs
+++ Moesif.Api.Test/ApiControllerTest.cs
@@ -18,8 +18,7 @@ namespace Moesif.Api.Test
     {
         private static ApiController controller;
 
-        [Fact]
-        public async Task TestAddEvent()
+        private EventModel CreateEvent()
         {
             // Parameters for the API call
             var reqHeaders = new Dictionary<string, string>();
@@ -45,7 +44,7 @@ namespace Moesif.Api.Test
                 }");
 
             var rspHeaders = new Dictionary<string, string>();
-            rspHeaders.Add("Date", "Tue, 23 Nov 2016 23:46:49 GMT");
+            rspHeaders.Add("Date", "Tue, 20 Aug 2019 23:46:49 GMT");
             rspHeaders.Add("Vary", "Accept-Encoding");
             rspHeaders.Add("Pragma", "no-cache");
             rspHeaders.Add("Expires", "-1");
@@ -88,9 +87,18 @@ namespace Moesif.Api.Test
                 Request = eventReq,
                 Response = eventRsp,
                 UserId = "my_user_id",
+                CompanyId = "my_company_id",
                 SessionToken = "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
                 Metadata = metadata
             };
+
+            return eventModel;
+        }
+
+        [Fact]
+        public async Task TestAddEvent()
+        {
+            EventModel eventModel = CreateEvent();
 
             Dictionary<string, string> rsp;
             // Perform API call
@@ -112,7 +120,12 @@ namespace Moesif.Api.Test
         public async Task TestAddBatchedEvents()
         {
             // Parameters for the API call
-            List<EventModel> body = ApiHelper.JsonDeserialize<List<EventModel>>("[{\"request\":{\"time\":\"2018-10-21T04:45:42.914\",\"uri\":\"https://api.acmeinc.com/items/reviews/testb\",\"verb\":\"PATCH\",\"api_version\":\"1.1.0\",\"ip_address\":\"61.48.220.123\",\"headers\":{\"Host\":\"api.acmeinc.com\",\"Accept\":\"*/*\",\"Connection\":\"Keep-Alive\",\"User-Agent\":\"Dalvik/2.1.0(Linux;U;Android5.0.2;C6906Build/14.5.A.0.242)\",\"Content-Type\":\"application/json\",\"Content-Length\":\"126\",\"Accept-Encoding\":\"gzip\"},\"body\":{\"items\":[{\"direction_type\":1,\"discovery_id\":\"fwfrf\",\"liked\":false},{\"direction_type\":2,\"discovery_id\":\"d43d3f\",\"liked\":true}]}},\"response\":{\"time\":\"2018-10-21T04:45:43.914\",\"status\":200,\"headers\":{\"Date\":\"Tue,23Aug201623:46:49GMT\",\"Vary\":\"Accept-Encoding\",\"Pragma\":\"no-cache\",\"Expires\":\"-1\",\"Content-Type\":\"application/json;charset=utf-8\",\"X-Powered-By\":\"ARR/3.0\",\"Cache-Control\":\"no-cache\",\"Arr-Disable-Session-Affinity\":\"true\"},\"body\":{\"Error\":\"InvalidArgumentException\",\"Message\":\"Missingfieldfield_a\"}},\"user_id\":\"mndug437f43\",\"session_token\":\"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\"},{\"request\":{\"time\":\"2018-10-21T04:46:42.914\",\"uri\":\"https://api.acmeinc.com/items/reviews/testc\",\"verb\":\"PATCH\",\"api_version\":\"1.1.0\",\"ip_address\":\"61.48.220.123\",\"headers\":{\"Host\":\"api.acmeinc.com\",\"Accept\":\"*/*\",\"Connection\":\"Keep-Alive\",\"User-Agent\":\"Dalvik/2.1.0(Linux;U;Android5.0.2;C6906Build/14.5.A.0.242)\",\"Content-Type\":\"application/json\",\"Content-Length\":\"126\",\"Accept-Encoding\":\"gzip\"},\"body\":{\"items\":[{\"direction_type\":1,\"discovery_id\":\"fwfrf\",\"liked\":false},{\"direction_type\":2,\"discovery_id\":\"d43d3f\",\"liked\":true}]}},\"response\":{\"time\":\"2018-10-21T04:46:43.914\",\"status\":200,\"headers\":{\"Date\":\"Tue,23Aug201623:46:49GMT\",\"Vary\":\"Accept-Encoding\",\"Pragma\":\"no-cache\",\"Expires\":\"-1\",\"Content-Type\":\"application/json;charset=utf-8\",\"X-Powered-By\":\"ARR/3.0\",\"Cache-Control\":\"no-cache\",\"Arr-Disable-Session-Affinity\":\"true\"},\"body\":{\"Error\":\"InvalidArgumentException\",\"Message\":\"Missingfieldfield_a\"}},\"user_id\":\"mndug437f43\",\"session_token\":\"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\"}]");
+            List<EventModel> body = new List<EventModel>();
+
+            EventModel eventModel = CreateEvent();
+
+            body.Add(eventModel);
+            body.Add(eventModel);
 
             Dictionary<string, string> rsp;
             // Perform API call
@@ -165,7 +178,8 @@ namespace Moesif.Api.Test
             var userModel = new UserModel()
             {
                 UserAgentString = "Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)",
-                UserId = "my_user_id",
+                UserId = "12345",
+                CompanyId = "67890",
                 Metadata = metadata,
                 ModifiedTime = DateTime.UtcNow
             };
@@ -189,7 +203,41 @@ namespace Moesif.Api.Test
         {
             Boolean isUpdated = false;
             // Parameters for the API call
-            List<UserModel> body = ApiHelper.JsonDeserialize<List<UserModel>>("[{\"modified_time\":\"2018-10-30T21:35:06.432\",\"ip_address\":\"0:0:0:0:0:0:0:1\",\"session_token\":\"23jdf0oszfexfqe[lwjfiefovprewv4d8ayrcdx8nu2ng]zfeeadedefx43f\",\"user_id\":\"my_user_id\",\"user_agent_string\":\"\",\"metadata\":{\"email\":\"johndoe@acmeinc.com\",\"string_field\":\"value_1\",\"number_field\":0,\"object_field\":{\"field_a\":\"value_a\",\"field_b\":\"value_b\"}},\"org_id\":\"86:5\",\"app_id\":\"46:8\"},{\"modified_time\":\"2018-10-30T21:35:06.432\",\"ip_address\":\"0:0:0:0:0:0:0:1\",\"session_token\":\"23jdf0oszfexfqe[lwjfiefovprewv4d8ayrcdx8nu2ng]zfeeadedefx43f\",\"user_id\":\"abc_user\",\"user_agent_string\":\"\",\"metadata\":{\"email\":\"johndoe@acmeinc.com\",\"string_field\":\"value_1\",\"number_field\":0,\"object_field\":{\"field_a\":\"value_a\",\"field_b\":\"value_b\"}},\"org_id\":\"86:5\",\"app_id\":\"46:8\"}]");
+            List<UserModel> body = new List<UserModel>();
+
+            Dictionary<string, object> metadata = new Dictionary<string, object>
+            {
+                {"email", "johndoe@acmeinc.com"},
+                {"string_field", "value_1"},
+                {"number_field", 0},
+                {"object_field", new Dictionary<string, string> {
+                    {"field_a", "value_a"},
+                    {"field_b", "value_b"}
+                    }
+                }
+            };
+
+            var userModelA = new UserModel()
+            {
+                UserId = "12345",
+                CompanyId = "67890",
+                Metadata = metadata,
+                ModifiedTime = DateTime.UtcNow
+            };
+
+
+            var userModelB = new UserModel()
+            {
+                UserId = "1234",
+                CompanyId = "6789",
+                Metadata = metadata,
+                SessionToken = "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
+                ModifiedTime = DateTime.UtcNow
+            };
+
+            body.Add(userModelA);
+            body.Add(userModelB);
+
 
             // Perform API call
             try
@@ -222,7 +270,7 @@ namespace Moesif.Api.Test
 
             var companyModel = new CompanyModel()
             {
-                CompanyId = "1",
+                CompanyId = "12345",
                 Metadata = metadata,
                 ModifiedTime = DateTime.UtcNow
             };
@@ -261,7 +309,7 @@ namespace Moesif.Api.Test
 
             var companyModelA = new CompanyModel()
             {
-                CompanyId = "1",
+                CompanyId = "12345",
                 Metadata = metadata,
                 ModifiedTime = DateTime.UtcNow
             };
@@ -269,7 +317,7 @@ namespace Moesif.Api.Test
 
             var companyModelB = new CompanyModel()
             {
-                CompanyId = "2",
+                CompanyId = "67890",
                 Metadata = metadata,
                 CompanyDomain = "CompanyDomain",
                 ModifiedTime = DateTime.UtcNow
